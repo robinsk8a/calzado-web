1e840b28d121c46cc1d869de0437560cbec1a435
robinsk8a <robinsk8a@gmail.com>
2024-02-26 14:27:00 -0500

    website barebones first commit

---
+++ src/App.vue
@@ -0,0 +1,60 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <div class="branding">
+      <img src="" alt="">
+      <h3 class="brand">Longos</h3>
+    </div>
+    <nav class="nav">
+      <li>Mayoristas</li>
+      <li>Constacto</li>
+    </nav>
+  </header>
+  <div class="hero">
+    <img src="./components/img/longos02.jpg" alt="" class="hero-img">
+    <div class="hero-section">
+      <h1 class="h-title">Calzado Longos</h1>
+      <p class="h-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatibus eaque earum laboriosam quidem, similique odit!</p>
+      <button class="h-c2a">Comprar en Linea</button>
+    </div>
+  </div>
+  <section class="store-section">
+    <div class="products">
+      <div class="prod-display">
+        <div class="display__wrap">
+          <img src="./components/img/longos01.jpg" alt="" class="display__img">
+        </div>
+      </div>
+      <div class="prod-card">
+        <div class="card__wrap">
+          <img src="" alt="" class="card__img">
+          <div class="card__content">
+            <div class="c-content__likes">
+              <p>6</p>
+              <img src="./components/img/longos01.jpg" alt="">
+            </div>
+            <div class="c-content__text">
+              <h2>Some shoes</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas est aperiam odit hic, dicta modi harum facere omnis nostrum corrupti ut cumque, ea quia dolore!</p>
+            </div>
+            <div class="c-content__customer">
+              <div class="customer-sizes">
+                <h3>Disponibilidad de tallas</h3>
+                <ul class="sizes">
+                  <li>38</li>
+                  <li>39</li>
+                  <li>40</li>
+                </ul>
+              </div>
+              <button>Haz tu pedido aquí</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </section>
+</template>
