90aad081d1daf6807291d31c8c3e7622d31a3a55
robinsk8a <robinsk8a@gmail.com>
2024-03-01 14:47:32 -0500

    Footer and product cards styling

--- src/App.vue
+++ src/App.vue
@@ -35,32 +35,45 @@ import "./assets/main.css";
       </div>
       <div class="prod-card">
         <div class="card__wrap">
-          <img src="" alt="" class="card__img">
+          <img src="./components/img/longos01.jpg" alt="" class="card__img">
           <div class="card__content">
+
             <div class="c-content__likes">
               <p>6</p>
-              <img src="./components/img/longos01.jpg" alt="">
+              <i class="fa-regular fa-heart"></i>
             </div>
             <div class="c-content__text">
-              <h2>Some shoes</h2>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas est aperiam odit hic, dicta modi harum facere omnis nostrum corrupti ut cumque, ea quia dolore!</p>
+              <h2 class="card__header">Some shoes</h2>
+              <p class="card__description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas est aperiam odit hic, dicta modi harum facere omnis nostrum corrupti ut cumque, ea quia dolore!</p>
             </div>
             <div class="c-content__customer">
               <div class="customer-sizes">
-                <h3>Disponibilidad de tallas</h3>
-                <ul class="sizes">
+                <h4>Disponibilidad de tallas</h4>
+                <ul class="customer__sizes">
                   <li>38</li>
                   <li>39</li>
                   <li>40</li>
                 </ul>
               </div>
-              <button>Haz tu pedido aquí</button>
+              <button class="c-content__button">
+                Haz tu pedido aquí
+              </button>
             </div>
           </div>
         </div>
       </div>
     </div>
-
+    <footer class="footer">
+      <div id="copyright" align="center"><small>&copy; 2024 - origenbylongos.com</small></div>
+      <ul class="footer-list">
+        <li>Outlet y Descuentos</li>
+        <li>Comprar en Pereira</li>
+        <li>Pedidos por mayor</li>
+        <li>Comprar en línea</li>
+        <li>Novedades</li>
+        <li>Contacto</li>
+      </ul>
+    </footer>
   </section>
 </template>
 
@@ -135,10 +148,7 @@ p {
   width: 45%;
   object-fit: cover;
 }
-.hero-img::after {
-  background: linear-gradient(to left, aliceblue, transparent);
-  z-index: 8;
-}
+
 .hero-section{
   max-width: 45rem;
   display: flex;
@@ -170,7 +180,7 @@ p {
   
 }
 .store-section{
-
+  
 }
 .products{
 
@@ -179,36 +189,109 @@ p {
 
 }
 .display__wrap{
-
+  width: 100%;
+  background-color: rgba(0, 0, 0, 0.4);
+  padding-inline: calc((100% - var(--max-width)) / 2);
+  display: flex;
+  justify-content: space-between;
 }
 .display__img{
-
+  height: 20svh;
+  padding: 2rem;
+  background-color: rgba(240, 248, 255, 0.6);
 }
-.prod-card{
-
+.prod-card {
+  background: rgba(240, 248, 255, 0.473);
+  border-block: solid 0.5rem rgba(255, 166, 0, 0.9);
+  width: 100%;
+  height: 70svh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 .card__wrap{
-
+  max-width: var(--max-width);
+  height: 80%;
+  margin: auto;
+  background-color: var(--white-origen);
+  border-radius: 2rem;
+  overflow: hidden;
+  display: flex;
+  gap: 2rem;
+  margin: auto;
 }
 .card__img{
-
+  height: 100%;
+  max-width: 45%;
+  object-fit: cover;
 }
 .card__content{
-
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  padding: 2.5rem 1rem;
 }
 .c-content__likes{
-
+  display: flex;
+  align-items: center;
+  gap: 0.6rem;
+  position: absolute;
+  top: 1.4rem;
+  right: 3rem;
+  font-family: var(--sans);
+  font-weight: 800;
 }
 .c-content__text{
-
+  
+}
+.card__header {
+  margin-bottom: 2rem;
+}
+.card__description{
+  padding-left: 1rem;
 }
 .c-content__customer{
-
+display: flex;
+justify-content: space-around;
+padding-inline: 1rem;
+margin-top: auto;
 }
 .customer-sizes{
-
+ display: flex;
+ flex-direction: column;
+ gap: 0.4rem;
+ color: rgb(131, 130, 130);
+ font-size: 80%;
 }
-.sizes{
-
+.customer__sizes{
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+.c-content__button {
+  background: var(--orange-origen);
+  padding: 0.2rem 1rem;
+  border-radius: 1rem;
+  font-size: 70%;
+  font-weight: 600;
+}
+.footer {
+  background-color: rgb(104, 103, 103);
+  display: flex;
+  align-items: flex-start;
+  justify-content: space-around;
+  padding-inline: calc((100% - var(--max-width)) / 2);
+  padding-block: 2rem;
+  color: var(--white-origen);
+}
+.copyright {
+}
+.footer-list {
+  padding-inline: 1rem;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 0.4rem;
+  border-left: solid 0.2rem ;
 }
 </style>
